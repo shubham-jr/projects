0060d810e9dba76a86a91eb1157f2fad601be59c
unknown <shubhampandeybhu21@gmail.com>
2022-03-01 11:32:28 +0530

    updated

--- Jr Prime/server.js
+++ Jr Prime/server.js
@@ -1,16 +1,33 @@
-const mongoose = require('mongoose');
-const app = require('./app.js');
-const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
-const DB = process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD);
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = require("./app.js");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 const port = process.env.PORT;
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-	useUnifiedTopology:true
-})
-.then(conn=>{console.log('database connected....')})
-.catch(err=>{console.log(err)});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log("database connected....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(port,'127.0.0.1',()=>{
-    console.log(`listening on port ${port}`);
-})
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.listen(port, "127.0.0.1", () => {
+  console.log(`listening on port ${port}`);
+});
